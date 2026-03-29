cc29580d35cbed0f0047e3ec69d82218c5148e08
amoshydra <8733840+amoshydra@users.noreply.github.com>
2026-03-30 03:48:02 +0800

    chore: allow connection from remote host

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: 5173,
+    allowedHosts: true,
     proxy: {
       "/api": {
         target: "http://localhost:3001",
